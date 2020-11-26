abaa13009967a806d42c54f435a813725468e8fe
matthewxvoorhees <matthew.crnich@gmail.com>
2020-11-26 13:35:41 -0700

    commit tree 43c33caed931c8e833f2c2fb754e012c64e82f39

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => selectImg(json));
+});
